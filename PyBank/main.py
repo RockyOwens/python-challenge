f457aad0a52d42253f83295e443c09ea24c13777
Rocky Owens <119636073+RockyOwens@users.noreply.github.com>
2023-01-09 17:14:15 -0600

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -85,10 +85,10 @@ print(printoutput)
 
 # Exporting to text file
 
-output_file = os.path.join('Analysis', 'PyBankAnalysis.txt')
+output_file = os.path.join('Analysis', 'pyBank_output.txt')
 
 
-PyBankAnalysis = open(output_file, "w")
+pyBankoutput = open(output_file, "w")
 
 line1 = "Financial Analysis"
 line2 = "------------------------------------------"
@@ -99,5 +99,5 @@ line6 = str(
     f"Greatest Increase in Profits: {greatest__inc_date} (${str(greatest_increase)})")
 line7 = str(
     f"Greatest Decrease in Profits: {greatest__dec_date} (${str(greatest_decrease)})")
-PyBankAnalysis.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(
+pyBankoutput.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(
     line1, line2, line3, line4, line5, line6, line7))
